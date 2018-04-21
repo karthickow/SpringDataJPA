b35beee33760fcf2a6b8498a366192de5fffbc31
karthickow <karthickkrishnan24@gmail.com>
2018-04-22 02:06:02 +0530

    Added password encoder

--- src/main/java/com/karthick/jpaonetomany/security/SecurityConfiguration.java
+++ src/main/java/com/karthick/jpaonetomany/security/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.karthick.jpaonetomany.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +25,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser(user).password(pass).roles(role);
+    String password = passwordEncoder().encode(pass);
+    auth.inMemoryAuthentication().withUser(user).password(password).roles(role);
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().antMatchers("/mystore/**").permitAll();
     http.authorizeRequests().antMatchers("/manage/**").hasRole(role).and().httpBasic();
+  }
+  
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
   }
 }
